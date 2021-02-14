8a52d5519beb788bf27a1ba7e4ae22910d8295c6
Max Wilson <wilson.max@gmail.com>
2021-02-13 18:51:49 -0800

    Cleanup and show how to use Hedgehog

--- Script1.fsx
+++ Script1.fsx
@@ -1,10 +1,5 @@
 // Here's a script for playing around with maze generation algorithms
 
-#r "nuget: Unquote, 5.0.0"
-#r "nuget: FsCheck"
-open Swensen.Unquote
-open FsCheck
-
 type CellCoords = CellCoords of int * int
 type ConnectionCoords = int
 type Direction = Up | Down | Left | Right
@@ -21,11 +16,7 @@ type Maze = {
     finish: CellCoords
     }
 let between a b x = a <= x && x <= b
-let M,N,m,n,dir = 3,4,2,4,Down
-let M,N,m,n,dir = 3,4,1,1,Down
-let M,N,m,n,dir = 3,4,2,1,Up
-let M,N,m,n,dir = 3,4,2,1,Down
-let M,N,m,n,dir = 3,4,2,4,Down
+
 let coordsOf M N (CellCoords (m,n), dir: Direction) : ConnectionCoords option =
     if between 1 M m && between 1 N n then
         match dir with
@@ -42,11 +33,7 @@ let coordsOf M N (CellCoords (m,n), dir: Direction) : ConnectionCoords option =
     else
         None
 let thunk x _ = x
-FsCheck.Check.Quick (fun (x: int, y: int) -> coordsOf 4 5 (CellCoords(x,y), Left) = coordsOf 4 5 (CellCoords(x,y-1), Right))
-FsCheck.Check.Quick (fun (x: int, y: int) -> coordsOf 4 5 (CellCoords(x,y), Up) = coordsOf 4 5 (CellCoords(x-1,y), Down))
-FsCheck.Check.Quick (fun (x: int, y: int) -> coordsOf 10 7 (CellCoords(x,y), Up) = coordsOf 10 7 (CellCoords(x-1,y), Down))
-let gridCoord (CellCoords(x,y)) =
-    (2*y), (2*x)
+
 (*
 #######
 #   # #
@@ -90,22 +77,30 @@ let renderMaze (maze:Maze) =
             else "##"
             ] |> join ""
         ] |> join "\n"
-let m = { M = 3; N = 4; walls = Array.create 17 Closed; start = CellCoords (3,3); finish = CellCoords(1, 1) }
-let r = System.Random()
-let m = { M = 3; N = 4; walls = Array.init 17 (fun _ -> [Open;Closed].[r.Next 2]); start = CellCoords (3,3); finish = CellCoords(1, 1) }
-"\n" + renderMaze m
 
 // draw a maze to Console.Out
 let draw (maze:Maze) =
     printfn "%s" (renderMaze maze)
 
-let maze = m
-isConnection (1, 7)
-isEntranceOrExit maze (1, 6)
+let clearMaze = { M = 3; N = 3; walls = Array.create 12 Open; start = CellCoords (3,3); finish = CellCoords(1, 1) }
+let blockedMaze = { M = 3; N = 4; walls = Array.create 17 Closed; start = CellCoords (3,3); finish = CellCoords(1, 1) }
+let r = System.Random()
+let randomMaze = { M = 3; N = 4; walls = Array.init 17 (fun _ -> [Open;Closed].[r.Next 2]); start = CellCoords (3,3); finish = CellCoords(1, 1) }
+clearMaze |> draw
+blockedMaze |> draw
+draw randomMaze
 
-let walls = [|
-    [|Closed; Open|]
-    [|Open; Closed|]
-    |]
+#r "nuget: Unquote, 5.0.0"
+open Swensen.Unquote
+#r "nuget: FsCheck"
+open FsCheck
+FsCheck.Check.Quick (fun (x: int, y: int) -> coordsOf 4 5 (CellCoords(x,y), Left) = coordsOf 4 5 (CellCoords(x,y-1), Right))
+FsCheck.Check.Quick (fun (x: int, y: int) -> coordsOf 4 5 (CellCoords(x,y), Up) = coordsOf 4 5 (CellCoords(x-1,y), Down))
+FsCheck.Check.Quick (fun (x: int, y: int) -> coordsOf 10 7 (CellCoords(x,y), Up) = coordsOf 10 7 (CellCoords(x-1,y), Down))
 
-"\n" + renderMaze { walls = Array2D.initBased 1 1 (fun i j -> walls.[i].[j]); start = CellCoords (3,3); finish = CellCoords(1, 1) }
+#r "nuget: Hedgehog, 0.10.0"
+open Hedgehog
+Property.check (property {
+    let! (x,y) = Gen.tuple (Gen.int (Range.linear 0 10))
+    return coordsOf 4 5 (CellCoords(x,y), Left) = coordsOf 4 5 (CellCoords(x,y-1), Right)
+    })
